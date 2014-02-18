5125d2ba00856d0aaf1dd21e0153c3a4a5280f6c
keimi <keimiokamoto@gmail.com>
2014-02-18 19:08:23 +0000

    initial commit

---
+++ ContactManager/src/com/keimiokamoto/main/java/meetingItem/MeetingImpl.java
@@ -0,0 +1,8 @@
+package com.keimiokamoto.main.java.meetingItem;
+
+/**
+ * Created by keimiokamoto on 18/02/2014.
+ */
+public class MeetingImpl {
+
+}
---
+++ ContactManager/src/com/keimiokamoto/test/java/meetingItem/MeetingImplTest.java
@@ -0,0 +1,8 @@
+package java.meetingItem;
+
+/**
+ * Created by keimiokamoto on 18/02/2014.
+ */
+public class MeetingImplTest {
+
+}

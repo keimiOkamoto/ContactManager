f5ad2d1f3ce817a3a8e7d07da61fe4c7d5f29bf5
keimi <keimiokamoto@gmail.com>
2014-02-21 14:21:13 +0000

    getName() returning null

--- ContactManager/src/com/keimiokamoto/test/src/meetingItem/ContactTest.java
+++ ContactManager/src/com/keimiokamoto/test/src/meetingItem/ContactTest.java
@@ -4,26 +4,37 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ContactTest {
+    Contact aContact = new ContactImpl();
+    Contact aContact1 = new ContactImpl();
+    Contact aContact2 = new ContactImpl();
 
     @Test
     public void shouldBeAbleToGetUniqueId() {
-        Contact aContact = new ContactImpl();
+
         int actual = aContact.getId();
         int expected = 0;
 
         assertEquals(expected, actual);
 
-        Contact aContact1 = new ContactImpl();
         int actual1 = aContact1.getId();
         int expected1 = 1;
 
         assertEquals(expected1, actual1);
 
-        Contact aContact2 = new ContactImpl();
+
         int actual2 = aContact2.getId();
         int expected2 = 2;
 
         assertEquals(expected2, actual2);
+    }
+
+    @Test
+    public void shouldBeAbleToGetName() {
+        String actual = aContact.getName();
+        String expected = "Adam";
+
+        assertEquals(expected,actual);
+
     }
 
 }
